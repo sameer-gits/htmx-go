df8a5bb4faa4d4faa93a12d9cbd2bc7bbc8988ce
sameer-gits <ms3054825@gmail.com>
2024-03-06 22:47:03 +0530

    so my mistake was that the folder in the directory is the package name... well best practice:
    so: goApplication/pkg/hello all the subsequent .go files should have package hello at the top
    
    changed api/index.go package handler to package api

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/sameer-gits/htmx-go/handler" // Import the index.go file from the htmx-go repository
+	"github.com/sameer-gits/htmx-go/api"
 )
 
 func main() {
-    http.HandleFunc("/", handler.Handler)
-    fmt.Println("Server is running on port 8080")
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", api.Handler)
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
