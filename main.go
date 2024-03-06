9db9caf21ebc9f58ae7d6b3bd8f86a7b770ca8d2
sameer-gits <ms3054825@gmail.com>
2024-03-06 23:58:38 +0530

    made routing

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 	"net/http"
 
 	"github.com/sameer-gits/htmx-go/api"
+	"github.com/sameer-gits/htmx-go/staticpages"
 )
 
 func main() {
 	http.HandleFunc("/", api.Handler)
+	http.HandleFunc("/page", staticpages.Page)
+	http.HandleFunc("/page2", staticpages.PageTwo)
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
