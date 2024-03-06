c2669376bf0226cc76e420160aa70769c176eb0f
sameer-gits <ms3054825@gmail.com>
2024-03-06 23:38:01 +0530

    basic html page

--- api/index.go
+++ api/index.go
@@ -1,10 +1,9 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
 )
  
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+  http.ServeFile(w, r, "index.html")
 }
